a3969b5ab327662f5d33b23728c28396b69a0a0d
orlovse <hajoff@gmail.com>
2022-05-13 20:54:02 +0300

    Add user types

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,13 +1,34 @@
 import { defineStore } from "pinia";
 
+enum UserRole {
+  admin = "admin",
+}
+
+type UserType = {
+  id: string;
+  avatar: string;
+  name: string;
+  role: UserRole;
+  email: string;
+  isDarkTheme: boolean;
+};
+
+type UserStateType = {
+  user: UserType | null;
+  isDarkTheme: boolean;
+};
+
 export const useUserStore = defineStore({
   id: "user",
-  state: () => ({
-    user: null,
-    isDarkTheme: true,
-  }),
+  state: () =>
+    ({
+      user: null,
+      isDarkTheme: true,
+    } as UserStateType),
   getters: {
     isAuthenticated: (state) => !!state.user,
+    isDarkTheme: (state) => state.user?.isDarkTheme || state.isDarkTheme,
+    isAdmin: (state) => state.user?.role === UserRole.admin,
   },
   actions: {
     switchTheme() {
