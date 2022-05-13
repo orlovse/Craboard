b6313008106b0fba41fe977482df070697e4570b
orlovse <hajoff@gmail.com>
2022-05-13 10:57:40 +0300

    Add drag and drop components

--- src/components/CustomDrag.vue
+++ src/components/CustomDrag.vue
@@ -1,7 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface IProps {
+  transferData: string;
+}
+
+const props = defineProps<IProps>();
+
+const onDrag = (event: DragEvent) => {
+  const dataTransfer = event.dataTransfer;
+
+  if (dataTransfer) {
+    dataTransfer.effectAllowed = "move";
+    dataTransfer.dropEffect = "move";
+
+    dataTransfer.setData("payload", JSON.stringify(props.transferData));
+  }
+};
+</script>
 
 <template>
-  <div></div>
+  <div
+    draggable="true"
+    @dragstart.self="onDrag"
+    @dragover.prevent
+    @dragenter.prevent
+  >
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/CustomDrop.vue
+++ src/components/CustomDrop.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const emit = defineEmits(["drop"]);
+
+const onDrop = (event: DragEvent) => {
+  const dataTransfer = event.dataTransfer;
+  if (dataTransfer) {
+    const transferData = JSON.parse(dataTransfer.getData("payload"));
+
+    emit("drop", transferData);
+  }
+};
+</script>
 
 <template>
-  <div></div>
+  <div @drop.stop="onDrop" @dragover.prevent @dragenter.prevent>
+    <slot></slot>
+  </div>
 </template>
 
 <style scoped></style>
