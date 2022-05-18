8304711149307062520939be81b56fca7d3e6e4f
orlovse <hajoff@gmail.com>
2022-05-18 23:19:04 +0300

    Add columns adding

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -6,11 +6,11 @@ import { storeToRefs } from "pinia";
 import { useBoardsStore } from "@/stores/boards";
 import BoardColumn from "@/components/BoardColumn.vue";
 import CustomSkeleton from "../components/CustomSkeleton.vue";
+import CustomInput from "../components/CustomInput.vue";
 
 const boardStore = useBoardsStore();
 const route = useRoute();
 const router = useRouter();
-console.log("route", route.query);
 
 const newColumnName = ref("");
 
@@ -30,6 +30,7 @@ const selectedBoard = computed(() => {
 
 const addNewColumn = () => {
   createColumnAction(newColumnName.value);
+  newColumnName.value = "";
 };
 
 const isTaskOpen = computed(() => {
@@ -60,13 +61,15 @@ const closeTaskModal = () => {
         :column="column"
         :columnIndex="columnIndex"
       />
-      <!-- <div class="column">
-        <input
+      <div class="column">
+        <CustomInput
           type="text"
+          placeholder="Add new column"
+          class="column-input"
           v-model="newColumnName"
           @keyup.enter="addNewColumn"
         />
-      </div> -->
+      </div>
     </div>
 
     <Transition name="fade" mode="out-in">
@@ -115,6 +118,10 @@ const closeTaskModal = () => {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.column-input {
+  width: 150px;
 }
 
 .fade-enter-active,
