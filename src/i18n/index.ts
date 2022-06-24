56049159feb1b0cb8a9c49c683008184531c24e8
orlovse <hajoff@gmail.com>
2022-06-24 15:19:26 +0300

    Add locale checking

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,19 @@
 import { createI18n } from "vue-i18n";
 import { en } from "./en";
 
+const rtlLocales = ["ar", "he"];
+
+const browserLocale = navigator.language.split("-")[0];
+
+const selectedLocale = browserLocale || "en";
+
+if (rtlLocales.includes(selectedLocale)) {
+  document.dir = "rtl";
+}
+
 export const i18n = createI18n({
-  locale: "en",
   allowComposition: true,
+  locale: selectedLocale,
   fallbackLocale: "en",
   messages: {
     en,
